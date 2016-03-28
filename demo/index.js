a9d0407c4bf98bb350044f3ed2f4fe8eb8a7e14e
Martin Bavio <mbavio@gmail.com>
2016-03-28 15:25:27 -0300

    Hello async magic

--- demo/index.js
+++ demo/index.js
@@ -3,7 +3,7 @@ Negan = require('../index.js');
 app = Negan('_build');
 
 app
-	.on('/', 'templates/index.html', {layout: 'layouts/default.html'})
-	.on('/blog', 'templates/blog.html')
+	// .on('/', 'templates/index.html', {layout: 'layouts/default.html'})
 	.on('/about', 'templates/about.html')
+  .on('/blog', 'templates/blog.html')
 	.build();
