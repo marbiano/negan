1fb28de1fdafb6af7bfd3bca393f11ae4d17ff02
Martin Bavio <mbavio@gmail.com>
2016-04-04 17:45:15 -0300

    Updated demo

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,13 @@
-Negan = require('../index.js');
+Negan = require('negan');
 
 app = Negan('_build');
 
 app
 	.on('/', 'templates/index.html', {layout: 'layouts/default.html'})
-	// .on('/about', 'templates/about.html')
- //  .on('/blog', 'templates/blog.html')
- //  .on('/css/main.css', 'assets/css/main.scss')
- //  .on('/css/static-lol.css', 'assets/css/static.css')
- //  .on('/js/test.js', 'assets/js/test.js')
- //  .on('/img/gugui.jpg', 'assets/img/gugui.jpg')
+	.on('/about', 'templates/about.html')
+  .on('/blog', 'templates/blog.html')
+  .on('/css/main.css', 'assets/css/main.scss')
+  .on('/css/static-lol.css', 'assets/css/static.css')
+  .on('/js/test.js', 'assets/js/test.js')
+  .on('/img/gugui.jpg', 'assets/img/gugui.jpg')
 	.build();
