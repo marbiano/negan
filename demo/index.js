e1a86c606474788908a9ee1919d36ba5c51bbd6b
Martin Bavio <mbavio@gmail.com>
2016-04-04 02:14:16 -0300

    More updates

--- demo/index.js
+++ demo/index.js
@@ -6,10 +6,8 @@ app
 	.on('/', 'templates/index.html', {layout: 'layouts/default.html'})
 	.on('/about', 'templates/about.html')
   .on('/blog', 'templates/blog.html')
-
   .on('/css/main.css', 'assets/css/main.scss')
   .on('/css/static-lol.css', 'assets/css/static.css')
   .on('/js/test.js', 'assets/js/test.js')
   .on('/img/gugui.jpg', 'assets/img/gugui.jpg')
-
 	.build();
