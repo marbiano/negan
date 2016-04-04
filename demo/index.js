55d9edb18d5d7f157c251655bcf680fd7a847f5d
Martin Bavio <mbavio@gmail.com>
2016-04-04 17:20:41 -0300

    Published 0.1.0

--- demo/index.js
+++ demo/index.js
@@ -4,10 +4,10 @@ app = Negan('_build');
 
 app
 	.on('/', 'templates/index.html', {layout: 'layouts/default.html'})
-	.on('/about', 'templates/about.html')
-  .on('/blog', 'templates/blog.html')
-  .on('/css/main.css', 'assets/css/main.scss')
-  .on('/css/static-lol.css', 'assets/css/static.css')
-  .on('/js/test.js', 'assets/js/test.js')
-  .on('/img/gugui.jpg', 'assets/img/gugui.jpg')
+	// .on('/about', 'templates/about.html')
+ //  .on('/blog', 'templates/blog.html')
+ //  .on('/css/main.css', 'assets/css/main.scss')
+ //  .on('/css/static-lol.css', 'assets/css/static.css')
+ //  .on('/js/test.js', 'assets/js/test.js')
+ //  .on('/img/gugui.jpg', 'assets/img/gugui.jpg')
 	.build();
